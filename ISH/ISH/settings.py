7c62f1624f5260b62a0af4a1766f49c7459caeb6
Benediktas Kaminskas <benediktas.kaminskas@proxama.com>
2017-03-30 12:42:12 +0100

    Changed django settings to allow pythonanywhere as a host

--- ISH/ISH/settings.py
+++ ISH/ISH/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wa8%-co*xgb^0)d=@f!fj_v099-^fsac@9hrc8zys%izh0$07*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'kaminskasb.pythonanywhere.com',
+]
 
 
 # Application definition
